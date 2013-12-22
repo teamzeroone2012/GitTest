7f3822e111a5ef7a8c74d1f36500deef3ce85a72
ZeroOne <teamzeroone2012@gmail.com>
2013-12-22 13:34:40 +0900

    Second Commit

--- src/com/icox/gittest/MainActivity.java
+++ src/com/icox/gittest/MainActivity.java
@@ -1,7 +1,8 @@
 package com.icox.gittest;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
 
 public class MainActivity extends Activity {
 
@@ -10,4 +11,12 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
 }
